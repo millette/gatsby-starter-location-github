4dc896d6e9a1337339d4cdff2cde88f753b56d55
Robin Millette <robin@millette.info>
2018-07-14 12:59:27 -0400

    Use links with forced language.

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,11 +9,26 @@ const IndexPage = () => (
     <h1>
       <FormattedMessage id='welcome' />
     </h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to='/page-2/'>
-      <FormattedMessage id='gopage2' />
-    </Link>
+    <p>
+      <FormattedMessage id='build' />
+    </p>
+    <ul>
+      <li>
+        <Link to='/page-2/'>
+          <FormattedMessage id='gopage2' />
+        </Link>
+      </li>
+      <li>
+        <Link to='/page-2/' lng='fr'>
+          francais, page 2
+        </Link>
+      </li>
+      <li>
+        <Link to='/page-2/' lng='en'>
+          english, page 2
+        </Link>
+      </li>
+    </ul>
   </Layout>
 )
 
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,14 +1,33 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { FormattedMessage } from 'react-intl'
+import { withIntl, Link } from '../i18n'
 
 import Layout from '../components/layout'
 
 const SecondPage = () => (
   <Layout>
-    <h1>Hi from the second page</h1>
+    <h1>
+      <FormattedMessage id='welcome2' />
+    </h1>
     <p>Welcome to page 2</p>
-    <Link to='/'>Go back to the homepage</Link>
+    <ul>
+      <li>
+        <Link to='/'>
+          <FormattedMessage id='goback' />
+        </Link>
+      </li>
+      <li>
+        <Link to='/' lng='fr'>
+          francais, accueil
+        </Link>
+      </li>
+      <li>
+        <Link to='/' lng='en'>
+          english, front
+        </Link>
+      </li>
+    </ul>
   </Layout>
 )
 
-export default SecondPage
+export default withIntl(SecondPage)
