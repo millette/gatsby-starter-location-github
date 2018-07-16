0aa6e52876e17c9260df4500dccc1d2f190e977a
Robin Millette <robin@millette.info>
2018-07-16 04:35:38 -0400

    Updates

---
+++ component---src-pages-page-2-js-f78527e824dd94b77a2f.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{209:function(e,t,a){"use strict";a.r(t);var n=a(1),o=a.n(n),c=a(218),l=a(225);t.default=function(){return o.a.createElement(l.a,null,o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(c.a,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-f78527e824dd94b77a2f.js.map
