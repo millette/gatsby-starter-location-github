e032c43a519f26e531ca2b652c3de68e659a82e7
Robin Millette <robin@millette.info>
2018-07-25 16:00:33 -0400

    Improve layout a bit.

--- src/components/github-user.js
+++ src/components/github-user.js
@@ -21,39 +21,41 @@ const GithubUser = props => {
         {/* if props.name is falsy, show props.login */}
         <h3 className='card-title'>{props.name || props.login}</h3>
         <a
+          role='button'
+          className='w-100 btn btn-warning btn-sm'
           target='_blank'
           rel='noopener noreferrer'
           href={`https://github.com/${props.login}`}
         >
-          {`https://github.com/${props.login}`}
+          {props.login} @ GitHub
         </a>
         {/* if props.bio is truthy, show it in a paragraph */}
         {props.bio && <p className='card-text'>{props.bio}</p>}
         <dl className='row'>
-          <dt className='col-6'>Location</dt>
-          <dd className='col-6'>{props.location}</dd>
+          <dt className='col-6 col-xl-4'>Location</dt>
+          <dd className='col-6 col-xl-8'>{props.location}</dd>
 
           {props.websiteUrl && (
             <Fragment>
-              <dt className='col-6'>Site web</dt>
-              <dd className='col-6'>{props.websiteUrl}</dd>
+              <dt className='col-6 col-xl-4'>Site web</dt>
+              <dd className='col-6 col-xl-8'>{props.websiteUrl}</dd>
             </Fragment>
           )}
 
           {props.email && (
             <Fragment>
-              <dt className='col-6'>Email</dt>
-              <dd className='col-6'>{props.email}</dd>
+              <dt className='col-6 col-xl-4'>Email</dt>
+              <dd className='col-6 col-xl-8'>{props.email}</dd>
             </Fragment>
           )}
 
-          <dt className='col-6'>Créé</dt>
-          <dd className='col-6'>{props.createdAt}</dd>
+          <dt className='col-6 col-xl-4'>Créé</dt>
+          <dd className='col-6 col-xl-8'>{props.createdAt}</dd>
 
           {props.company && (
             <Fragment>
-              <dt className='col-6'>Compagnie</dt>
-              <dd className='col-6'>{props.company}</dd>
+              <dt className='col-6 col-xl-4'>Compagnie</dt>
+              <dd className='col-6 col-xl-8'>{props.company}</dd>
             </Fragment>
           )}
 
@@ -63,9 +65,9 @@ const GithubUser = props => {
           {props.starredRepositories &&
             props.starredRepositories.length > 0 && (
             <Fragment>
-              <dt className='col-6'>Étoiles</dt>
+              <dt className='col-6 col-xl-4'>Étoiles</dt>
               {/* Here, we want it displayed, even if it's 0 although the previous test would prevent 0 */}
-              <dd className='col-6'>
+              <dd className='col-6 col-xl-8'>
                 {props.starredRepositories.length} dépots
               </dd>
             </Fragment>
@@ -74,14 +76,14 @@ const GithubUser = props => {
           {props.repositoriesContributedTo &&
             props.repositoriesContributedTo.length > 0 && (
             <Fragment>
-              <dt className='col-6'>Contribué à</dt>
-              <dd className='col-6'>
+              <dt className='col-6 col-xl-4'>Contribs</dt>
+              <dd className='col-6 col-xl-8'>
                 {props.repositoriesContributedTo.length} dépots
               </dd>
               {props.languages && (
                 <Fragment>
-                  <dt className='col-6'>Langages</dt>
-                  <dd className='col-6'>{props.languages}</dd>
+                  <dt className='col-6 col-xl-4'>Langages</dt>
+                  <dd className='col-6 col-xl-8'>{props.languages}</dd>
                 </Fragment>
               )}
             </Fragment>
