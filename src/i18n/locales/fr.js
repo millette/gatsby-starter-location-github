f8aedec729bf168775bcb6d8a7d6f294d1e6b598
Robin Millette <robin@millette.info>
2018-07-30 23:21:05 -0400

    French strings touchups.

--- src/i18n/locales/fr.js
+++ src/i18n/locales/fr.js
@@ -5,5 +5,7 @@ module.exports = {
   gopage2: 'Aller à la Page 2',
   build: 'À votre tour de faire quelque chose de merveilleux.',
   welcome2: 'Voici la Page 2',
-  goback: "Aller à la Page d'accueil"
+  goback: 'Aller à la Page d’accueil',
+  'jumbotron.text':
+    '[FR] Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.'
 }
