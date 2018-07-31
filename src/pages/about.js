784f06706bf9e90838334a2fea7930d6beea6b35
Robin Millette <robin@millette.info>
2018-07-31 14:50:34 -0400

    Move bottin to frontpage; add about page stub.

---
+++ src/pages/about.js
@@ -0,0 +1,63 @@
+// npm
+import React, { Fragment, Component } from 'react'
+import { FormattedMessage } from 'react-intl'
+import { withIntl, Link } from '../i18n'
+
+// self
+import Layout from '../components/layout'
+import Footer from '../components/footer'
+
+const About = () => (
+  <Layout>
+    <div className='container'>
+      <h1>
+        <FormattedMessage id='welcome2' />
+      </h1>
+
+      <ul>
+        <li>
+          <Link to='/'>
+            <FormattedMessage id='goback' />
+          </Link>
+        </li>
+        <li>
+          <Link to='/' lng='fr'>
+            francais, accueil
+          </Link>
+        </li>
+        <li>
+          <Link to='/' lng='en'>
+            english, front
+          </Link>
+        </li>
+
+        <li>
+          <Link to='/was-index/' lng='fr'>
+            francais, was-index
+          </Link>
+        </li>
+        <li>
+          <Link to='/was-index/' lng='en'>
+            english, was-index
+          </Link>
+        </li>
+
+        <li>
+          <Link to='/was-page-2/' lng='fr'>
+            francais, was-page-2
+          </Link>
+        </li>
+        <li>
+          <Link to='/was-page-2/' lng='en'>
+            english, was-page-2
+          </Link>
+        </li>
+      </ul>
+    </div>
+    <Footer>
+      <p className='card-text'>Marvoulous</p>
+    </Footer>
+  </Layout>
+)
+
+export default withIntl(About)
---
+++ src/pages/was-index.js
@@ -0,0 +1,53 @@
+// npm
+import React from 'react'
+import { FormattedMessage } from 'react-intl'
+import { withIntl, Link } from '../i18n'
+
+// self
+import Layout from '../components/layout'
+import Footer from '../components/footer'
+
+const IndexPage = () => (
+  <Layout header>
+    <div className='container'>
+      <h1>
+        <FormattedMessage id='welcome' />
+      </h1>
+      <p>
+        <FormattedMessage id='build' />
+      </p>
+      <ul>
+        <li>
+          <Link to='/new-front/' lng='fr'>
+            francais, new-front
+          </Link>
+        </li>
+        <li>
+          <Link to='/new-front/' lng='en'>
+            english, new-front
+          </Link>
+        </li>
+        <li>
+          <Link to='/page-2/'>
+            <FormattedMessage id='gopage2' />
+          </Link>
+        </li>
+        <li>
+          <Link to='/page-2/' lng='fr'>
+            francais, page 2
+          </Link>
+        </li>
+        <li>
+          <Link to='/page-2/' lng='en'>
+            english, page 2
+          </Link>
+        </li>
+      </ul>
+    </div>
+    <Footer>
+      <p className='card-text'>Marvoulous</p>
+    </Footer>
+  </Layout>
+)
+
+export default withIntl(IndexPage)
