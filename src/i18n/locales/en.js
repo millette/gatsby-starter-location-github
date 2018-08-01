3fa246fa2517953ea10d83398c8e5f70d377aefd
Robin Millette <robin@millette.info>
2018-08-01 19:11:46 -0400

    New strings for about page; add override instructions.

--- src/i18n/locales/en.js
+++ src/i18n/locales/en.js
@@ -1,12 +1,30 @@
+/* Don’t edit this file.
+ * See the custom/locales/ directory
+ * and create the appropriate en.js and fr.js files.
+ * Strings found in custom/locales/ files will override default strings.
+ *
+ * N’éditez pas ce fichier.
+ * Voir le répertoire custom/locales/
+ * pour y créer les fichiers en.js et fr.js appropriés.
+ * Les chaines trouvées dans les fichiers de custom/locales/
+ * remplaceront les chaines par défaut.
+ */
 module.exports = {
-  title: 'RolloDevs — Iran (en)',
+  title: 'RolloDevs — Iran developpers',
   subtitle: 'Your locally sourced developper directory',
   'about.title': 'About us',
+  'about.col1.html': `
+<p>English bla bla.</p>
+<p>And second paragraph.</p>
+`,
+  'about.front': 'The directory',
+  'about.contact': 'Questions?',
+  'about.col2.html': `
+<p>English bla bla for column 2.</p>
+<p>And second paragraph.</p>
+<p>And More, as you wish.</p>
+  `,
   'contact.title': 'Contact us',
-  hello: 'Hi people',
-  welcome: 'Welcome to your new Gatsby site.',
-  gopage2: 'Go to page 2',
-  build: 'Now go build something great.',
-  welcome2: 'Welcome to page 2',
-  goback: 'Go back to the homepage'
+  'jumbotron.text':
+    'Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.'
 }
--- src/i18n/locales/fr.js
+++ src/i18n/locales/fr.js
@@ -1,14 +1,34 @@
+/* Don’t edit this file.
+ * See the custom/locales/ directory
+ * and create the appropriate en.js and fr.js files.
+ * Strings found in custom/locales/ files will override default strings.
+ *
+ * N’éditez pas ce fichier.
+ * Voir le répertoire custom/locales/
+ * pour y créer les fichiers en.js et fr.js appropriés.
+ * Les chaines trouvées dans les fichiers de custom/locales/
+ * remplaceront les chaines par défaut.
+ */
 module.exports = {
-  title: 'RolloDevs — Iran (fr)',
+  title: 'RolloDevs — développeurs d’Iran',
   subtitle: 'Votre source de développeurs locaux',
   'about.title': 'À propos',
+  'about.col1.html': `
+<p>Francais bla bla.</p>
+<p>Et deuxième paragraphe.</p>
+`,
+  'about.front': 'Allez au répertoire',
+  'about.contact': 'Des questions?',
+  'about.col2.html': `
+<p>Français bla bla pour la colonne 2.</p>
+<p>Et deuxième paragraphe.</p>
+<ul>
+  <li>Même</li>
+  <li>une</li>
+  <li>liste</li>
+</ul>
+`,
   'contact.title': 'Contactez nous',
-  hello: 'Bonjour monde',
-  welcome: 'Votre nouveau site gatsby vous accueille.',
-  gopage2: 'Aller à la Page 2',
-  build: 'À votre tour de faire quelque chose de merveilleux.',
-  welcome2: 'Voici la Page 2',
-  goback: 'Aller à la Page d’accueil',
   'jumbotron.text':
     '[FR] Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.'
 }
