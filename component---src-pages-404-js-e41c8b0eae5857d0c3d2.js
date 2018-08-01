dda08cc7902395d75b77b360c98934766efbbf01
Robin Millette <robin@millette.info>
2018-07-31 22:14:41 -0400

    Updates

---
+++ component---src-pages-404-js-e41c8b0eae5857d0c3d2.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{213:function(e,t,a){"use strict";a.r(t);var n=a(1),l=a.n(n),s=a(220),c=a(221);t.default=function(){return l.a.createElement(s.a,null,l.a.createElement("div",{className:"container"},l.a.createElement("h1",null,"NOT FOUND"),l.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness.")),l.a.createElement(c.a,null,l.a.createElement("p",{className:"card-text"},"Marvoulous")))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-e41c8b0eae5857d0c3d2.js.map
