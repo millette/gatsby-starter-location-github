fdf4160eb5c78d8d4f9de4c36e077c5fe4bcbcce
Robin Millette <robin@millette.info>
2018-08-01 15:50:05 -0400

    Remove last default message.

--- src/components/header.js
+++ src/components/header.js
@@ -28,7 +28,7 @@ const Header = ({ pageContext, siteTitle, subTitle }) => (
             <p className='lead'>
               <FormattedMessage
                 id='jumbotron.text'
-                defaultMessage='Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.'
+                defaultMessage666='Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.'
               />
             </p>
           </div>
