f1ca270ea20fe76139e63740f14513fe07c87b5a
Robin Millette <robin@millette.info>
2018-08-02 12:56:44 -0400

    Component index.

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import Footer from './footer'
+import GithubUser from './github-user'
+import Header from './header'
+import LanguageSwitch from './language-switch'
+import Layout from './layout'
+import Nav from './nav'
+
+export { Footer, GithubUser, Header, LanguageSwitch, Layout, Nav }
