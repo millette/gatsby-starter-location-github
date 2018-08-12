307df5e4b3e1f0b0eae69aa02c3c536a0d1564d0
Robin Millette <robin@millette.info>
2018-08-12 18:22:49 -0400

    Use npm-git-links in gatsby config.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,28 +1,61 @@
-const { gitHead } = require('./utils')
+// const { name, headHash, version, versionTag, versionHash } = require('./utils')
+// const version = require('./utils')
+
+// console.log(headHash, version, versionHash)
+// throw 'ciao'
+
+// console.log(new Date().toISOString())
+// console.log('DIRNAME:', __dirname)
+// console.log('CWD:', process.cwd())
+
+const cwd = process.cwd()
 
 try {
   const { themeType, themeName } = require('./custom/config.js')
   let themeDir
   switch (themeType) {
     case 'custom':
-      themeDir = `${__dirname}/custom/scss`
+      themeDir = `${cwd}/custom/scss`
       break
 
     case 'bootswatch':
       if (themeName) {
-        themeDir = `${__dirname}/node_modules/bootswatch/dist/${themeName}`
+        themeDir = `${cwd}/node_modules/bootswatch/dist/${themeName}`
         break
       }
 
     // eslint-disable-next-line no-fallthrough
     default:
-      themeDir = `${__dirname}/scss/default`
+      themeDir = `${cwd}/scss/default`
   }
+
+  console.log('OKOKOK')
+  const { ret, Doit } = require('npm-git-links')
+  // console.log('DOIT', Doit)
+
+  const aa = new Doit(ret.repository)
+  ret.repoUrl = aa.browse
+
+  aa.version = ret.headHash
+  ret.headUrl = aa.browse
+
+  aa.version = ret.versionHash
+  ret.versionUrl = aa.browse
+
+  console.log('RET:', ret)
+
+  /*
+  console.log('AA1', aa1)
+  console.log('AA2', aa2)
+  console.log('AA3', aa3)
+  */
+
+  // const version = ret
 
   module.exports = {
     pathPrefix: '/gatsby-starter-location-github',
     siteMetadata: {
-      gitHead,
+      version: ret,
       language: {
         fallback: 'en'
       }
@@ -32,7 +65,7 @@ try {
       {
         resolve: 'gatsby-plugin-sass',
         options: {
-          includePaths: [themeDir, `${__dirname}/node_modules/bootstrap`],
+          includePaths: [themeDir, `${cwd}/node_modules/bootstrap`],
           precision: 8 // required by bootstrap
         }
       },
@@ -42,7 +75,7 @@ try {
         resolve: 'gatsby-source-filesystem',
         options: {
           name: 'data',
-          path: `${__dirname}/custom/data/`
+          path: `${cwd}/custom/data/`
         }
         /*
       },
@@ -56,9 +89,7 @@ try {
     ]
   }
 } catch (e) {
-  console.error(
-    `Did you forget to create the ${__dirname}/custom/config.js file?`
-  )
+  console.error(`Did you forget to create the ${cwd}/custom/config.js file?`)
   console.error(e.toString())
-  throw e
+  // throw e
 }
