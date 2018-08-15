f3da529da739f6784f2edae0f288e5cd9b6091fa
Robin Millette <robin@millette.info>
2018-08-15 13:47:18 -0400

    Re-re-add purgecss workaround.

--- src/components/radios.js
+++ src/components/radios.js
@@ -21,12 +21,20 @@ class Radios extends Component {
   }
 
   render () {
+    // FIXME: workaround purgecss and missing classes
+    // See also the whitelist option for 'gatsby-plugin-purgecss' in gatsby-config.js
+    false && (
+      <span className='text-primary text-secondary text-success text-danger text-warning text-info btn-primary btn-secondary btn-success btn-danger btn-warning btn-info btn-outline-primary btn-outline-secondary btn-outline-success btn-outline-danger btn-outline-warning btn-outline-info' />
+    )
+
     return (
       <div className='form-group row'>
         <div
-          className={`col-6 col-sm-12 col-form-label text-${this.props.color}`}
+          className={`col-6 col-sm-12 col-form-label text-truncate text-${
+            this.props.color
+          }`}
         >
-          <small>{this.props.title}</small>
+          {this.props.title}
         </div>
         <div className='col-6 col-sm-12 btn-group btn-group-sm btn-group-toggle'>
           {this.buttons.map(({ key, txt }, i) => (
