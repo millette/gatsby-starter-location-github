b5b40d99f65703eea27999aa298cc1222e38aa68
Robin Millette <robin@millette.info>
2018-09-18 05:36:45 -0400

    Handle language sets in layout.

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,8 +8,16 @@ import 'typeface-roboto'
 import { Social, Header, Nav } from '.'
 import './layout.scss'
 
-const Layout = ({ messages, header, children }, ctx) => {
-  const pageContext = ctx.language || {}
+// const Layout = ({ pageContext, messages, header, children }, ctx) => {
+// const pageContext = ctx.language || {}
+// const Layout = ({ pageContext, messages, header, children }) => {
+const Layout = (props, ctx) => {
+  const { messages, header, children } = props
+  const pageContext = props.pageContext || ctx.language || {}
+  // console.log('LAYOUT PAGECONTEXT:', pageContext)
+  // console.log('LAYOUT LC:', languageSet)
+  // console.log('LAYOUT ctx:', ctx)
+  // console.log('LAYOUT PAGECONTEXT:', pageContext)
   return (
     <Fragment>
       <Social messages={messages} pageContext={pageContext} />
